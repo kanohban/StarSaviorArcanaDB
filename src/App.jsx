bf4879c4e74e507be43d75eda96646ff76e79ee3
ss-administrator <koreamusume@gmail.com>
2025-12-01 22:15:56 +0900

    v1.0.1: 이벤트 이름, 조회수 추가

--- src/App.jsx
+++ src/App.jsx
@@ -142,6 +142,11 @@ function App() {
                 <p>이 페이지는 게임 '스타 세이비어'의 비영리 팬 프로젝트입니다.</p>
                 <p>프로젝트에 사용된 모든 자산, 데이터, 이미지 및 텍스트의 소유권은 STUDIOBSIDE 에 있습니다.</p>
                 <p>© STUDIOBSIDE Co. Ltd All Rights Reserved.</p>
+                <div className="visitor-counter" style={{ marginTop: '10px' }}>
+                    <a href="https://myhits.vercel.app">
+                        <img src="https://myhits.vercel.app/api/hit/https%3A%2F%2Fkanohban.github.io%2FStarSaviorArcanaDB%2F?color=blue&label=hits&size=small" alt="hits" />
+                    </a>
+                </div>
             </footer>
         </div>
     )
