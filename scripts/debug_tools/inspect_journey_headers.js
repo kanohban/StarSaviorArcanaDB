1ca76931f1a81160e976f01bc72ca0ae72d6eef2
Junyeong0218 <hippo2003@naver.com>
2026-01-15 03:14:28 +0900

    update data: 2026-01-15 03:14:27

--- scripts/debug_tools/inspect_journey_headers.js
+++
@@ -1,8 +0,0 @@
-const XLSX = require("xlsx");
-const path = "./import/game_data.xlsx";
-const wb = XLSX.readFile(path);
-const sheet = wb.Sheets["Journey"];
-const data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-
-console.log("Headers:", data[0]);
-console.log("First row:", data[1]);
